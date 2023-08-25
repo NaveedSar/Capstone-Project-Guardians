3446f8e9ffd21264a4bac5425687a13ef21394c9
NaveedSar <Naveed.s100@gmail.com>
2023-08-25 16:12:27 -0400

    updating project

--- src/test/java/tek/capstone/guardians/runner/TestRunner.java
+++ src/test/java/tek/capstone/guardians/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		tags = "@Regression",
+		tags = "@ReturnOrder",
 		features = ("classpath:features"),
 		glue = "tek.capstone.guardians",
 		dryRun = false,
